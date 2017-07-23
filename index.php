31cb7746d875ff25ebacfbf6676293d5305aa40a
Rafa Huelin <it@engisa.com>
2017-07-23 11:30:56 +0100

    Copy content from static files into the theme files

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<div class="grid-container">
+    <header class="grid-x grid-padding-x">
+        <div class="large-6 cell">
+            <img src="./img/logo.jpg">
+        </div>
+        <div class="large-6 cell">
+            <ul class="menu simple main-nav">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="index.html">Services</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="grid-x grid-padding-x showcase">
+        <div class="large-12 cell">
+            <div class="callout secondary">
+                <h1>Discount Clothing</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at elementum massa. Fusce vehicula, ex eu porttitor tincidunt, ante arcu pulvinar tortor, eu pretium tellus dui vel turpis. Sed mollis mi nec iaculis condimentum. Aenean efficitur mauris eu ligula suscipit, sed maximus neque placerat. Fusce bibendum lorem elit, non semper neque viverra sit amet. Donec pellentesque blandit lacus quis pulvinar. Nunc sit amet auctor urna, a eleifend dui.</p>
+                <button class="button">Start Shopping</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-x grid-padding-x">
+        <div class="large-8 medium-8 cell">
+            <div class="grid-x grid-padding-x products">
+                <div class="large-4 medium-4 small-12 cell product">
+                    <h3>Blue Shirt</h3>
+                    <h4>$9.99</h4>
+                    <img src="./img/shirt_blue_white.jpg">
+                    <button class="button">Details</button>
+                </div>
+                <div class="large-4 medium-4 small-12 cell product end">
+                    <h3>Red Shirt</h3>
+                    <h4>$19.99</h4>
+                    <img src="./img/shirt_red.jpg">
+                    <button class="button">Details</button>
+                </div>
+                <div class="large-4 medium-4 small-12 cell product end">
+                    <h3>Grey Shirt</h3>
+                    <h4>$11.99</h4>
+                    <img src="./img/shirt_grey.jpg">
+                    <button class="button">Details</button>
+                </div>
+                <div class="large-4 medium-4 small-12 cell product end">
+                    <h3>Orange Shirt</h3>
+                    <h4>$9.99</h4>
+                    <img src="./img/shirt_orange.jpg">
+                    <button class="button">Details</button>
+                </div>
+                <div class="large-4 medium-4 small-12 cell product end">
+                    <h3>Black Shirt</h3>
+                    <h4>$9.99</h4>
+                    <img src="./img/shirt_black.jpg">
+                    <button class="button">Details</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="large-4 medium-4 cell">
+            <div class="callout">
+                <h3>Categories</h3>
+                <ul class="menu vertical">
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Pants</a></li>
+                    <li><a href="#">Hats</a></li>
+                    <li><a href="#">Shoes</a></li>
+                </ul>
+            </div>
+            <br>
+            <div class="callout">
+                <h5>Sidebar heading</h5>
+                <p>Sed viverra ex quis purus fermentum dignissim. Sed consequat, purus vel fermentum fringilla, felis ipsum efficitur ipsum, ut aliquet lorem diam in turpis. Vestibulum eu pellentesque ante. Suspendisse quis urna in nisi efficitur mattis fermentum vel nisi. Pellentesque porta diam orci.</p>
+                <a href="http://foundation.zurb.com/sites/docs/" class="small button">Go to Foundation Docs</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <p>&copy; 2017 &mdash; Ecommerce</p>
+</footer>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
